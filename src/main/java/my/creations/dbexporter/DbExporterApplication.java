6040da99c5ab5071f1a22b8156f187ae92909bd2
Alex_Lewonas <alewonas@gmail.com>
2023-12-07 01:31:58 -0500

    add base code

---
+++ src/main/java/my/creations/dbexporter/DbExporterApplication.java
@@ -0,0 +1,13 @@
+package my.creations.dbexporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbExporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbExporterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/creations/dbexporter/DbExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package my.creations.dbexporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbExporterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
