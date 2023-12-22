cd2f4e12d21f276dcc63d422faabe0ef40cf13cd
Alex_Lewonas <alewonas@gmail.com>
2023-12-21 22:23:24 -0500

    edits and and docs

--- src/main/java/my/creations/dbexporter/config/JdbcTemplateConfig.java
+++ src/main/java/my/creations/dbexporter/config/JdbcTemplateConfig.java
@@ -7,20 +7,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+/**
+ * Configuration class for setting up a Spring JdbcTemplate.
+ */
 @Configuration
 public class JdbcTemplateConfig {
 
     private final DataSource dataSource;
 
+    /**
+     * Constructor for JdbcTemplateConfig.
+     *
+     * @param dataSource The DataSource to be used by the JdbcTemplate.
+     */
     @Autowired
     public JdbcTemplateConfig(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+    /**
+     * Creates and configures a JdbcTemplate bean using the provided DataSource.
+     *
+     * @return A configured JdbcTemplate bean.
+     */
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource);
     }
-
-
 }
